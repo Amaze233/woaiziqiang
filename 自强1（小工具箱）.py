bc4fd536a874de350d8b758cf5daa43b4e73d458
繁星若尘 <55312345+Amaze233@users.noreply.github.com>
2019-09-19 18:03:39 +0800

    Create 自强1（小工具箱）.py
    大佬饶过我吧，萌新尽力了

---
+++ 自强1（小工具箱）.py
@@ -0,0 +1,54 @@
+X = input("欢迎使用小工具箱，请根据以下提示信息进行操作\n"
+          "输入‘1’进行字符串输入并加密\n"
+          "输入‘2’进行字符串输入及字典制作\n"
+          "输入‘3’进行txt文件读取及二维码生成与保存:")
+if X is '1':
+    try:
+        import base64
+        str1 = input("欢迎使用字符串加解密系统\n"
+                     "请输入需加密的字符串：")
+
+        str2 = base64.b64encode(str1.encode("utf-8"))
+        print("加密后的字符串：", str2)
+        str3 = base64.b64decode(str2).decode("utf-8")
+        print("重新解密的字符串：", str3)
+    except :
+        print("输入非法字符，请重新输入！")
+elif X is '2':
+    try:
+        x = input('欢迎使用python字典制作、转换与反转系统\n'
+                  '请按说明输入字典的键与值\n'
+                  '注意:\n'
+                  '输入字典的键和值时请务必按对应顺序输入\n'
+                  '并且相邻的键或值之间请以空格隔开!!!!!!\n'
+                  '请输入字典的键：')
+        y = input('请输入字典的值：')
+        list1 = x.split(" ")
+        list2 = y.split(" ")
+        the_dict = dict(zip(list1, list2))
+        print("您所输入的字典为：", the_dict)
+        import json
+        json_str = json.dumps(the_dict)
+        print("所输字典转换的JSON字符串为：", json_str)
+        print("JSON字符串的类型为：", type(json_str))
+        the_dict_alter = dict(zip(the_dict.values(), the_dict.keys()))
+        print("反转后的字典为：", the_dict_alter)
+        print("反转后字典的类型为", type(the_dict_alter))
+    except:
+        print("输入非法字符，请重新输入！")
+elif X is "3":
+        i = input("欢迎使用QRcode生成系统\n"
+                  "请将要转化的txt文档放入此py文档的同一类目底下\n"
+                  "注意！生成的QRcode也将保存于同一类目下\n"
+                  "请输入要读取的文件名：")
+        file = open(i, "r")
+        data = file.read()
+        print(data)
+        import qrcode
+        img = qrcode.make(data=data)
+        img.show()
+        img.save("自强.jpg")
+
+
+
+
